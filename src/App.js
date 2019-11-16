d78aef48f5202b7bacb3929d735bc022f6a0f2ba
myke47 <47667530+myke47@users.noreply.github.com>
2019-11-15 19:23:33 -0700

    refactored routes and wrapped components in Provider

--- src/App.js
+++ src/App.js
@@ -6,6 +6,10 @@ import data from './data';
 import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
+import ProductContext from "./contexts/ProductContext";
+import CartContext from "./contexts/CartContext";
+import Products from './components/Products';
+
 
 function App() {
 	const [products] = useState(data);
@@ -16,26 +20,24 @@ function App() {
 	};
 
 	return (
+
+		<ProductContext.Provider
+			value={{ products, addItem }}
+			>
+			<CartContext.Provider value={cart}>
 		<div className="App">
 			<Navigation cart={cart} />
 
 			{/* Routes */}
-			<Route
-				exact
-				path="/"
-				render={() => (
-					<Products
-						products={products}
-						addItem={addItem}
-					/>
-				)}
-			/>
+			<Route exact path="/" component={Products} />
 
 			<Route
 				path="/cart"
-				render={() => <ShoppingCart cart={cart} />}
+				component={ShoppingCart}
 			/>
 		</div>
+		</CartContext.Provider>
+		</ProductContext.Provider>
 	);
 }
 
