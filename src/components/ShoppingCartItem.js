fdfdcf9b2fc8744443675be45219ff6977c65307
myke47 <47667530+myke47@users.noreply.github.com>
2019-11-19 21:21:05 -0700

    error persistent in removeItem function

--- src/components/ShoppingCartItem.js
+++ src/components/ShoppingCartItem.js
@@ -1,15 +1,18 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import ProductContext from "../contexts/ProductContext";
 
-const Item = props => {
+const Item = ({ id, image, price, title }) => {
+	const  removeItem  = useContext(ProductContext);
+
 	return (
 		<div className="shopping-cart_item">
-			<img src={props.image} alt={`${props.title} book`} />
+			<img src={image} alt={`${title} book`} />
 
 
 			<div>
-				<h1>{props.title}</h1>
-				<p>$ {props.price}</p>
-				<button>Remove from cart</button>
+				<h1>{title}</h1>
+				<p>$ {price}</p>
+				<button id={id} onClick={() => removeItem(id)}>Remove from cart</button>
 			</div>
 		</div>
 	);
